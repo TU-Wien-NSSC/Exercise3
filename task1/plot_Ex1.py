7c6942c6549c0fa27c3b553313eacecb71101cb3
Alice <alidec99@gmail.com>
2025-01-06 19:24:59 +0100

    Update plot_Ex1.py
    y-axis is logaritmic

--- task1/plot_Ex1.py
+++ task1/plot_Ex1.py
@@ -16,20 +16,19 @@ with open("residuals.txt", "r") as file:
 
 # Plot residuals
 plt.figure(figsize=(10, 6))
-plt.plot(iterations, residuals, label="Residuals", marker="o", linestyle="-", color="blue")
+plt.semilogy(iterations, residuals, label="Residuals", marker="o", linestyle="-", color="blue")
 plt.xlabel("Iteration")
 plt.ylabel("Residual")
 plt.title("Convergence of Residuals")
 plt.grid(True)
-plt.savefig("residuals_plot.png")
-plt.show()
+plt.savefig("PlotResidual.png")
 
 # Plot errors in A-norm
 plt.figure(figsize=(10, 6))
-plt.plot(iterations, errors, label="Error (A-norm)", marker="x", linestyle="--", color="red")
+plt.semilogy(iterations, errors, label="Error (A-norm)", marker="x", linestyle="--", color="red")
 plt.xlabel("Iteration")
 plt.ylabel("Error (A-norm)")
 plt.title("Convergence of Errors in A-Norm")
 plt.grid(True)
-plt.savefig("errors_plot.png")
+plt.savefig("PlotError.png")
 plt.show()
